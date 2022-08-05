5954b187f86b0a7da8c158e46542f6294c15b45a
finleyKim <g_kimmm@naver.com>
2022-08-05 18:51:18 +0900

    #1 [ADD] MainViewController - SearchBar, Filter, ListView

---
+++ MyLibrary/Entities/AlertActionConvertible.swift
@@ -0,0 +1,13 @@
+//
+//  AlertActionConvertible.swift
+//  MyLibrary
+//
+//  Created by Finley on 2022/08/05.
+//
+
+import UIKit
+
+protocol AlertActionConvertible {
+    var title: String { get }
+    var style: UIAlertAction.Style { get }
+}
