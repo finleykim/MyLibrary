8da3248efe2cfc22b5fd023b6f13bafa64335a6d
finleyKim <g_kimmm@naver.com>
2022-08-05 19:43:12 +0900

    #4 [FEAT] 네트워크추가 (API, Network)

--- MyLibrary/Network/API.swift
+++ MyLibrary/Network/API.swift
@@ -5,4 +5,24 @@
 //  Created by Finley on 2022/08/05.
 //
 
-import Foundation
+import RxSwift
+
+struct SearchBookAPI {
+    static let scheme = "https"
+    static let host = "dapi.kakao.com"
+    static let path = "/v3/search/"
+    
+    func searchBook(query: String) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = SearchBookAPI.scheme
+        components.host = SearchBookAPI.host
+        components.path = SearchBookAPI.path + "book"
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "size", value: "25")
+        ]
+        
+        return components
+    }
+}
--- MyLibrary/Network/Network.swift
+++ MyLibrary/Network/Network.swift
@@ -5,4 +5,38 @@
 //  Created by Finley on 2022/08/05.
 //
 
-import Foundation
+import RxSwift
+
+class SearchBookNetwork {
+    private let session: URLSession
+    let api = SearchBookAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func searchBook(query: String) -> Single<Result<Book, SearchNetworkError>> {
+        guard let url = api.searchBook(query: query).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 974defb9863e9c41e3f9ec6a213475a0", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let bookData = try JSONDecoder().decode(Book.self, from: data)
+                    return .success(bookData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
+}
+
